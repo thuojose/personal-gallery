06ce743e69ec2c4810150af93b0c41b21ed61239
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 18:52:07 +0300

    added test.py for image

--- photos/tests.py
+++ photos/tests.py
@@ -45,3 +45,38 @@ class LocationTestClass(TestCase):
         '''
         self.nairobi.save_location()
         self.nairobi.delete_location()
+
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new category and saving it
+        self.new_category= Category(name = 'Design')
+        self.new_category.save_category()
+
+        # Creating a new location and saving it
+        self.new_location= Location(name = 'Nairobi')
+        self.new_location.save_location()
+
+        self.new_image= Image(image='image.jpeg',name = 'Test Image',description = 'This is a random test Image description',category = self.new_category,location=self.new_location)
+        self.new_image.save()
+
+        # self.new_image.category.add(self.new_category)
+        # self.new_image.category.add(self.new_location)
+
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+
+    def test_save_method(self):
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    def test_delete_method(self):
+        self.new_image.save_image()
+        self.new_image.delete_image()

b91ef3ff1f694d99ef189e63bddf99b1472c9044
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 16:37:14 +0300

    installation of pip

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from .models import Image
+
 
 # Create your views here.
+def index(request):
+
+    photos=Image.get_photos()
+
+    return render(request,'index.html',{"photos":photos})
+

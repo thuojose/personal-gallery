f6e55890a712cc7b9c867ef3512d0ccf7c4674ec
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 18:53:48 +0300

    added model class for image

--- photos/models.py
+++ photos/models.py
@@ -36,3 +36,29 @@ class Location(models.Model):
     # def update_location(cls,location,update):
     #     updated = cls.objects.filter(image_name=location).update(name=update)
     #     return updated
+    
+class Image(models.Model):
+    image=models.ImageField(upload_to ='images/')
+    name=models.CharField(max_length =30)
+    description=models.TextField()
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    location=models.ForeignKey(Location,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_photos(cls):
+
+        photos=cls.objects.all()
+        return photos
+
+    @classmethod
+    def get_image_by_id(cls,image_id):
+        image=cls.objects.get(id=image_id)
+        return image

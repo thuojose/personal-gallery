2426ebedea73b19fb77b8019eed328de9d6d5287
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 18:40:11 +0300

    added model class for location

--- photos/models.py
+++ photos/models.py
@@ -18,3 +18,21 @@ class Category(models.Model):
 
     def delete_category(self):
         self.delete()
+
+class Location(models.Model):
+    name=models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    # @classmethod
+    # def update_location(cls,location,update):
+    #     updated = cls.objects.filter(image_name=location).update(name=update)
+    #     return updated

e3b7b0a5c576ce0e315874c4345e04dd98858b7f
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 18:25:30 +0300

    created model class for category

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()

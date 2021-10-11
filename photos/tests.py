d303f844c5c097464811472ceca863b60aab2a33
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 18:44:31 +0300

    test.py for location

--- photos/tests.py
+++ photos/tests.py
@@ -24,3 +24,24 @@ class CategoryTestClass(TestCase):
         self.design.save_category()
         self.design.delete_category()
 
+class LocationTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.nairobi=Location(name='Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.nairobi.save_location()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_delete_method(self):
+        '''
+        Function that tests whether a location can be deleted
+        '''
+        self.nairobi.save_location()
+        self.nairobi.delete_location()

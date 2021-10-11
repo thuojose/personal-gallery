8e07197a14a5b9a905d44d4aaef8f4cfeb73d095
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 18:44:05 +0300

    test.py for category

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Category
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.design=Category(name='Design')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.design,Category))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.design.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+    def test_delete_method(self):
+
+
+
+        self.design.save_category()
+        self.design.delete_category()
+

6b1288c172ab754b4fefdb5ac13f9e59bf371be8
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 18:47:22 +0300

    added image detail in view.py

--- photos/views.py
+++ photos/views.py
@@ -31,6 +31,7 @@ def past_photos(request,past_date):
         return redirect(today_photos)
     return render(request,'all-photos/past-photos.html',{"date":date})
 
+
 def location(request,location_id):
     photos=Image.objects.filter(location_id=location_id)
 
@@ -40,3 +41,14 @@ def category(request,category_id):
     photos=Image.objects.filter(category_id=category_id)
 
     return render(request,'category.html',{"photos":photos})
+
+
+def imagedetails(request,image_id):
+    try:
+        image = Image.objects.get(id=image_id)
+    except DoesNotExist:
+         raise Http404()
+    return render(request,"image.html",{"image":image})
+
+def copy_image(from_model, to_model):
+    to_model.image.save(from_model.image.url.split('/')[-1],from_model.image.file,save=True)

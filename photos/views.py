f27be0b858a6aa2920c6569d8638164b440423cc
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-11 17:46:31 +0300

    added html for index

--- photos/views.py
+++ photos/views.py
@@ -16,3 +16,18 @@ def today_photos(request):
 
 
     return render(request,'all-photos/today-photos.html',{"date":date})
+
+
+def past_photos(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    if date == dt.date.today():
+        return redirect(today_photos)
+    return render(request,'all-photos/past-photos.html',{"date":date})
+
